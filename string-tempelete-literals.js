4e5527b88e5d4af99c70385d53101dac76822971
USER 1 <gchkumar999@gmail.com>
2024-05-27 18:14:30 +0530

    prat

---
+++ string-tempelete-literals.js
@@ -0,0 +1,9 @@
+
+// string-tempelete-literals : we are placing variable in to a string is called as string tempelete literal 
+
+
+
+const year=1995
+
+const result =` My year is ${year}`
+console.log(result)
