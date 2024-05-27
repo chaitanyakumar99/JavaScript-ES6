0167101c1a971817ff24d0dc1b543dfc745d3a79
USER 1 <gchkumar999@gmail.com>
2024-05-27 18:16:52 +0530

    prat

---
+++ spread-operator.js
@@ -0,0 +1,24 @@
+
+//spread operator : Alllow us quckily and copy all or part of an existing array or object into another array or object.
+
+
+// Array:
+const a=[100,200,300,400,500]
+
+result=[...a]
+console.log(result)
+
+//object:
+const b={name:"Rahul",age:25,loc:"vijayawada"}
+value={...b}
+console.log(value)
+
+
+
+
+
+
+
+
+
+
