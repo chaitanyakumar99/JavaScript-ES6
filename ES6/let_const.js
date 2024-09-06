693b82645ac1139802a9643cb969de6d81c52888
USER 1 <gchkumar999@gmail.com>
2024-09-06 23:11:28 +0530

    txt

--- ES6/let_const.js
+++ ES6/let_const.js
@@ -1,7 +1,31 @@
-//let const provides block-level-scope variables declared 
-//inside a block cannot be accessed from outside the block
+// //let const provides block-level-scope variables declared 
+// //inside a block cannot be accessed from outside the block
 
-{
-    let x=2;
-    console.log(x)
+// //block scope
+
+// {
+//     let x=2;
+//     console.log(x)
+// }
+
+// //function scope
+
+// function hi(){
+//     const a=25
+//     console.log(a)
+// }
+// hi()
+
+//Global scope
+
+let x=2;
+let y=20;
+
+function text(){
+
+    console.log(x);
+    console.log(y);
+    
 }
+
+text()
