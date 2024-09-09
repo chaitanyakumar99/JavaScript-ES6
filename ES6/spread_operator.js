a64b81dcb8c54d4cd16d313ac9cd95aecd1cda83
USER 1 <gchkumar999@gmail.com>
2024-09-09 12:55:19 +0530

    txt

--- ES6/spread_operator.js
+++ ES6/spread_operator.js
@@ -1,5 +1,18 @@
+//spread operator:- extract and copy into single array or a object
+
+//array
+
 let a=[1,2,3,4,5]
 let b=[6,7,8,9,10]
 
 let c=[...a,...b]
 console.log(c)
+
+
+//object
+
+const v1={name:"rahul",lastname:"gandhi"}
+const v2={name1:"Arun",lastname1:"Kumar"}
+const v3={...v1,...v2}
+console.log(v3)
+
