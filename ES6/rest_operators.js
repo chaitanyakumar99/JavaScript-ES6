b30c3230ace346699faa34e2a6c5fbd716900ef6
USER 1 <gchkumar999@gmail.com>
2024-09-09 13:13:39 +0530

    txt

--- ES6/rest_operators.js
+++ ES6/rest_operators.js
@@ -1,3 +1,6 @@
+//rest operator:-The rest operator in JavaScript (...) is used to collect multiple elements
+// or properties into a single array or object. 
+
 function grd(firstname,...othernames){
 
     console.log(`"Hello," ${firstname}`)
